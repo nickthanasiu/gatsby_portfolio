ccce59bcd0eede9701d3f825a7fe42d540e0af75
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2019-04-30 14:55:57 -0400

    Fix config for webpack alias resolve

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,15 +29,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-alias-imports`,
-      options: {
-        alias: {
-          "@components": path.resolve(__dirname, 'src/components')
-        },
-        extensions: []
-      }
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
