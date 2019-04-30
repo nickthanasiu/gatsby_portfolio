2b5f818af4d0c236416613fde5f70e3945f187ff
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2019-04-30 14:21:23 -0400

    Add alias support to webpack via gatsby-plugin-alias-imports

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "@components/Layout"
+import SEO from "@components/SEO"
+
+const About = () => (
+    <Layout>
+        <SEO title="About" />
+        <h1>
+            This is the about page!
+        </h1>
+    </Layout>
+)
+
+export default About
