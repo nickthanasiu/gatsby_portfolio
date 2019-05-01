e63171569af7816a65b58ff714c0f7a11c74b677
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2019-05-01 15:00:02 -0400

    Convert header component styling to styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nick Thanasiu Portfolio`,
+    description: `Web dev portfolio for Nick Thanasiu`,
+    author: `Nicholas Thanasiu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +29,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
